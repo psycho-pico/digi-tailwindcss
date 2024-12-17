5def2dd411a7688843ff3655e91ed1a7223ce6d5
Yohan <yohanes.hadi@pegadaian.co.id>
2024-12-17 15:33:51 +0700

    init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'custom-blue': '#1da1f2',
+                'custom-red': '#e0245e',
+            },
+        },
+    },
+    plugins: [],
+};
